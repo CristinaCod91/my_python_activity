f4f1707fd78d21f35f821c30442bef3162ccdcd8
CristinaCod91 <102391768+CristinaCod91@users.noreply.github.com>
2022-06-01 10:18:24 +0100

    My first code 1.2.5

---
+++ my_first_code.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+print("My name is Cristina")
+print("The date is 01/06/22")
+
+
+# In[5]:
+
+
+# Write a program to analyse real estate data
+# Create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Add all the sales together 
+total_sales = a + b + c + d + e
+
+# View the output
+print(total_sales)
+
+
+# In[6]:
+
+
+# Create a new variable
+# Calculate the average sales by dividing the total number of sales by 5
+avg_sales = total_sales / 5
+
+# View the output
+print(avg_sales)
+
+
+# In[7]:
+
+
+# create a new variable
+# Calculate the price of property C as sales - commission
+property_price_c = (67000 - 6500)
+
+# View the output
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
